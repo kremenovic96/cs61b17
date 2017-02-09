de13603b41bc3e71337c15ec1ead55bbf9de386f
kremenovic96 <ranko.kremenovic@gmail.com>
2017-02-09 09:29:29 +0100

    first part of proj1b finished(founded bug in buggy implementation)

---
+++ proj1b/TestArrayDeque1B.java
@@ -0,0 +1,68 @@
+/**
+ * Created by ranko on 2/8/17.
+ */
+import static org.junit.Assert.*;
+import org.junit.Test;
+import java.util.Random;
+public class TestArrayDeque1B {
+    @Test
+    public void testAddFirst(){
+        StudentArrayDeque<Integer> student = new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> solution = new ArrayDequeSolution<>();
+        Random rn = new Random();
+        for(int i = 0;i<10; i++){
+            int rnd = rn.nextInt();
+            student.addFirst(rnd);
+            solution.addFirst(rnd);
+        }
+        for(int i = 0;i<10; i++){
+            assertEquals(student.get(i), solution.get(i));
+        }
+
+    }
+    @Test
+    public void testAddLast(){
+        StudentArrayDeque<Integer> student = new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> solution = new ArrayDequeSolution<>();
+        Random rn = new Random();
+        for(int i = 0;i<10;i++){
+            int rnd = rn.nextInt();
+            student.addLast(rnd);
+            solution.addLast(rnd);
+        }
+        for(int i =0;i<10;i++){
+            assertEquals(student.get(i), solution.get(i));
+        }
+    }
+    @Test
+    public void testRemoveFirst(){
+        StudentArrayDeque<Integer> student = new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> solution = new ArrayDequeSolution<>();
+        Random rn = new Random();
+        for(int i =0;i<10;i++){
+            int rnd = rn.nextInt();
+            student.addFirst(rnd);
+            solution.addFirst(rnd);
+
+        }
+        for(int i = 0;i<10; i++){
+            assertEquals(student.removeFirst(),solution.removeFirst());
+        }
+    }
+    @Test
+    public void testRemoveLast(){
+        StudentArrayDeque<Integer> student = new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> solution = new ArrayDequeSolution<>();
+        Random rn = new Random();
+        for(int i =0;i<10;i++){
+            int rnd = rn.nextInt();
+            student.addFirst(rnd);
+            solution.addFirst(rnd);
+        }
+        for(int i = 0; i < 10; i++){
+            assertEquals(student.removeLast(),solution.removeLast());
+        }
+
+    }
+
+}
